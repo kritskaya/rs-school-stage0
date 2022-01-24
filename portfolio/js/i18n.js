b057fe3707e9c9958862b11af1d0e80270fcb33c
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-01-24 16:19:41 +0300

    fix: menu margin in case of translation to russian

--- portfolio/js/i18n.js
+++ portfolio/js/i18n.js
@@ -2,6 +2,7 @@
 const langContainer = document.querySelector(".lng-list");
 const langItems = document.querySelectorAll(".lng-list-link");
 const dateI18nElements = document.querySelectorAll("[data-i18n]");
+const menu = document.querySelector(".header-nav");
 
 langContainer.addEventListener("click", translate);
 
@@ -17,6 +18,14 @@ function translate(event) {
 			let dataValue = elem.getAttribute("data-i18n");
 			elem.innerHTML = i18Obj[target.innerHTML][dataValue];
 		});
+
+		if (target.innerHTML === "ru") {
+			menu.style.marginLeft = "700px";
+			menu.style.width = "570px";
+		} else {
+			menu.style.marginLeft = "780px";
+			menu.style.width = "500px";
+		}
 	}
 }
 
