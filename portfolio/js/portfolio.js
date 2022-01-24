6add5e011cad8db6dcc9853a5c1c1a378f5b0477
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-01-24 16:04:49 +0300

    feat: implement translation function

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -1,15 +1,22 @@
 const portfolioBtnContainer = document.querySelector(".portfolio-btns");
 const portfolioBtns = document.querySelectorAll(".portfolio .btn");
+const images = document.querySelectorAll(".portfolio-img");
 
 portfolioBtnContainer.addEventListener("click", changeImage);
 
 function changeImage(event) {
 	
 	if (event.target.classList.contains("btn")) {
-		let clicked = event.target
+		let clicked = event.target;
+
 		portfolioBtns.forEach((elem) => {
 			elem.classList.add("btn-transparent");
 			clicked.classList.remove("btn-transparent");
 		});
+
+		images.forEach((img, index) => {
+			img.src = `./assets/img/${clicked.dataset.season}/${index + 1}.jpg`;
+		});
 	}
 }
+
