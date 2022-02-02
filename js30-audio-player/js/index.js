89c951a813a752aed105f9df39aa7666bc0e092d
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-02-02 16:59:41 +0300

    feat: define current track duration

--- js30-audio-player/js/index.js
+++ js30-audio-player/js/index.js
@@ -3,6 +3,12 @@ const audio = document.querySelector('audio');
 const playBtn = document.querySelector('.play-btn');
 let played = false;
 
+let minDuration = Math.floor(audio.duration / 60);
+let secDuration = Math.floor(audio.duration % 60);
+
+const durationSpan = document.querySelector('.duration-time');
+durationSpan.innerHTML = minDuration + ":" + secDuration;
+
 playBtn.addEventListener("click", function () {
 	
 	if (!played) {
