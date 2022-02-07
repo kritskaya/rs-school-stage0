3819e4b3ca9e67e641580a1d7022688a7c90ce13
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-02-07 15:33:22 +0300

    feat: implement getData function for a page onload

---
+++ js30-gallery/js/index.js
@@ -0,0 +1,3 @@
+import {getData} from "./loadImages.js";
+
+getData();
---
+++ js30-gallery/js/loadImages.js
@@ -0,0 +1,24 @@
+export async function getData() {
+	const url = "https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=m5rdFYPyYCesIEQg2bMvsLOIEZwBhK5nw2W7FrjsCn8";
+	const res = await fetch(url);
+	let data = await res.json();
+
+	const result = data.results;
+	console.log(Array.isArray(result));
+	
+	result.forEach(element => {
+		console.log(element.urls.regular);
+		addImage(element.urls.regular);
+	});	
+	
+};
+
+const container = document.querySelector(".main .container");
+
+const addImage = (url) => {
+	let image = document.createElement('div');
+	image.classList.add("gallery-image");
+	image.style.backgroundImage = `url(${url})`;
+	
+	container.append(image);
+}
