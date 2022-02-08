9923b1f3063d1c9048066465a2108647a0edb172
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-02-08 12:11:43 +0300

    feat: add search by keyword function

--- js30-gallery/js/search.js
+++ js30-gallery/js/search.js
@@ -2,11 +2,15 @@ const searchInput = document.querySelector(".search-input");
 
 export const subscribeSearchInput = () => {
 	searchInput.addEventListener("keydown", (event) => {
-		alert(event.key);
+		if (event.key === "Enter") {
+			let keyword = searchInput.value;
+			getData(keyword);
+		}
 	})
 }
 
 export async function getData(keyword) {
+		
 	const url = `https://api.unsplash.com/search/photos?query=${keyword}&per_page=30&orientation=landscape&client_id=m5rdFYPyYCesIEQg2bMvsLOIEZwBhK5nw2W7FrjsCn8`;
 	const res = await fetch(url);
 	let data = await res.json();
@@ -20,6 +24,8 @@ export async function getData(keyword) {
 };
 
 const container = document.querySelector(".main .container");
+
+
 
 const addImage = (url) => {
 	let image = document.createElement('div');
