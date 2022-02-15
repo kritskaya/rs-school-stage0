95eb0639e90a52315ee13588095a605434528b81
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-02-15 12:02:41 +0300

    feat: finish flip styles

--- js30-memory-game/js/playProcess.js
+++ js30-memory-game/js/playProcess.js
@@ -2,10 +2,21 @@ const gameField = document.querySelector(".game");
 
 export const subscribeCardClick = () => {
 	gameField.addEventListener("click", flipCard);
+
+	const cards = document.querySelectorAll('.memory-card');
+	cards.forEach(card => card.addEventListener('click', () => {
+		flipCard.classList.toggle("flip");
+	}));
 }
 
 function flipCard(event) {
 	if (event.target.classList.contains("back")) {
+		let flipCard = event.target.parentElement;
+		flipCard.classList.toggle("flip");
+	}
+
+	
+	if (event.target.classList.contains("back-face")) {
 		let flipCard = event.target.parentElement;
 		flipCard.classList.toggle("flip");
 	}
